58311120d390bc6353b2875929c2825d379cf9bc
Rajesh Kalidandi <kalidandiirajesh@gmail.com>
2024-09-01 15:58:32 +0530

    Description of your changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import SampleComponent from './components/SampleComponent';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 const App = () => {
@@ -13,6 +16,9 @@ const App = () => {
                 <Header />
                 <Switch>
                     <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/dashboard" component={Dashboard} />
                     <Route path="/" component={SampleComponent} />
                 </Switch>
                 <Footer />
